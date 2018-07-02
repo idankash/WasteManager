38a4e24e7b1712876c61a835f6e7869e456057d1
Lior Baraban <oracleilb@gmail.com>
2018-07-02 18:11:58 +0300

    first

---
+++ EntityTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            using (BusinessLogic bl = new BusinessLogic())
+            {
+                
+                List<Bin> binList = bl.GetAllBins();
+
+                foreach (Bin bin in binList)
+                {
+                    Console.WriteLine($"Trash Id=  { bin.BinId}  Address= { bin.CityAddress}  { bin.StreetAddress}   { bin.StreetNumber} ");
+                }
+                Console.WriteLine("================");
+
+
+                Bin singleBin = bl.GetBin(1);
+                Console.WriteLine("Id = {0}, Old Capacity = {1}", singleBin.BinId, singleBin.CurrentCapacity);
+                singleBin.CurrentCapacity += 20;
+                bl.UpdateBin(singleBin,DateTime.Now);
+                Console.WriteLine("Id = {0}, New Capacity = {1}", singleBin.BinId, singleBin.CurrentCapacity);
+
+     
+            }
+            */
+
+            WasteSimulator ws = new WasteSimulator(30);
+
+            ws.FillAllBinsRandomly();
+
+            
+
+            
+        }
+    }
+}
