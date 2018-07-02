7f771723233bb97db3a951b97468eae8b7b666e6
Lior Baraban <oracleilb@gmail.com>
2018-07-02 20:39:01 +0300

    Rebase Project Structure

---
+++ WasteSimulator/Program.cs
@@ -0,0 +1,50 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasteSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            using (BusinessLogic bl = new BusinessLogic())
+            {
+                
+                List<Bin> binList = bl.GetAllBins();
+
+                foreach (Bin bin in binList)
+                {
+                    Console.WriteLine($"Trash Id=  { bin.BinId}  Address= { bin.CityAddress}  { bin.StreetAddress}   { bin.StreetNumber} ");
+                }
+                Console.WriteLine("================");
+
+
+                Bin singleBin = bl.GetBin(1);
+                Console.WriteLine("Id = {0}, Old Capacity = {1}", singleBin.BinId, singleBin.CurrentCapacity);
+                singleBin.CurrentCapacity += 20;
+                bl.UpdateBin(singleBin,DateTime.Now);
+                Console.WriteLine("Id = {0}, New Capacity = {1}", singleBin.BinId, singleBin.CurrentCapacity);
+
+     
+            }
+            */
+
+            WasteSimulator ws = new WasteSimulator(30);
+
+            //ws.FillAllBinsRandomly();
+            List<Bin> bins = ws.test();
+
+            foreach(Bin bin in bins)
+            {
+                Console.WriteLine(bin.BinId);
+            }
+       
+            
+        }
+    }
+}
---
+++ WasteSimulator/WasteSimulator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL;
+using DAL;
+
+namespace WasteSimulator
+{
+    internal class WasteSimulator
+    {
+
+        public DateTime DestinationDateTime { get; set; }
+        public DateTime SourceDateTime { get; set; }
+
+        Random rand = new Random();
+
+
+        public WasteSimulator()
+        {
+
+        }
+
+        public WasteSimulator(int days)
+        {
+            DestinationDateTime = DateTime.Now.AddDays(days);
+            SourceDateTime = DateTime.Now;
+        }
+
+        public List<Bin> test()
+        {
+            try
+            {
+                using(BusinessLogic bl = new BusinessLogic())
+                {
+                    List<Bin> bins = bl.GetAllBins();
+                    return bins;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public void FillAllBinsRandomly()
+        {
+            while ((Convert.ToBoolean(SourceDateTime.Date.CompareTo(DestinationDateTime.Date))))
+            {
+                using (BusinessLogic bl = new BusinessLogic())
+                {
+                    List<Bin> binList = bl.GetAllBins();
+                    
+                    for (int i = 0; i < 12; i++)
+                    {
+                        // It means the client want to throw his garbage.. 
+
+
+                        foreach (Bin bin in binList)
+                        {
+                            if (rand.Next(0, 2) == 1)
+                            {
+                                int maxWaste = (int)bl.GetMaxCapacityByBinType(bin.BinTypeId);
+                                bin.CurrentCapacity += rand.Next(1, maxWaste / 12);
+
+                                bl.UpdateBin(bin, SourceDateTime);
+                            }
+                        }
+
+                        SourceDateTime = SourceDateTime.AddHours(2);
+                    }
+
+                    Truck truck = bl.GetTruck(1);
+
+                    foreach (Bin bin in binList)
+                    {
+                        truck.CurrentCapacity += bin.CurrentCapacity;
+                        bl.UpdateTruck(truck);
+
+                        bin.CurrentCapacity = 0;
+                        bl.UpdateBin(bin, SourceDateTime);
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+
